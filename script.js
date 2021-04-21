447e76195b4b987a3f4778c129cae5b24cf32904
SimoneSaramondi <simo98sara@gmail.com>
2021-04-21 15:37:28 +0200

    numeri random + alert

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
+
+// CREAZIONE NUMERI RANDOM CON VISIONE TRAMITE ALERT
+var numeriRandom = [];
+
+function numRand(){
+    return Math.floor(Math.random() * 10 ) + 1;
+}
+
+for(var i = 0; i < 5; i++){
+    numeriRandom[i] = numRand();
+}
+
+alert(numeriRandom);
+
+// TIMER DI TOT (30) SECONDI 
